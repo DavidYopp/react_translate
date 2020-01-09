cec285f7f73b6667c331c3eeee1a0fa39ecc096e
Gdraven99 <DavidJYopp@gmail.com>
2020-01-08 19:56:41 -0500

    Initial commit for the react translate app default create-react-app files removed index.js and app.js added

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = { language: 'english' };
+  render() {
+    return <div className="ui container">App</div>
+  }
+}
+
+export default App;
